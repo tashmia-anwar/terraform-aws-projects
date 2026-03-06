2d0e2f79983a9497b357adca336ae75eca9d3c7a
Tashmia Anwar <tashmia.anwar20@gmail.com>
2026-03-06 15:52:24 -0600

    Create private subnet

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "private" {
     vpc_id = aws_vpc.main.id
-    cidr_block = 10.0.1.0/24
+    cidr_block = "10.0.1.0/24"
 
     tags = {
         env = "test"
