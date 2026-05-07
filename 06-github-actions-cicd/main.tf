b06a47de71f476f45cbb06b060ac915db1e02b6a
Tashmia Anwar <tashmiaanwar@Tashmias-MacBook-Pro.local>
2026-05-07 17:22:21 -0500

    Add GitHub Actions Terraform pipeline

---
+++ 06-github-actions-cicd/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create simple S3 bucket for pipeline to validate and plan against
+resource "aws_s3_bucket" "this" {
+    bucket = var.bucket_name
+
+    tags = {
+        Environment = var.environment
+        ManagedBy = "terraform"
+    }
+}
